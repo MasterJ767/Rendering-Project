8b0f195ed63b8646857b7ebfd39cbd26e1242c1d
MasterJ767 <jon.arundel@hotmail.com>
2023-03-25 15:46:17 +0000

    Add index buffers

--- ZenithCore/Model.h
+++ ZenithCore/Model.h
@@ -20,7 +20,12 @@ namespace Zenith {
 				static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
 			};
 
-			Model(Device &device, const std::vector<Vertex> &vertices);
+			struct Builder {
+				std::vector<Vertex> vertices{};
+				std::vector<uint32_t> indices{};
+			};
+
+			Model(Device &device, const Model::Builder& builder);
 			~Model();
 
 			Model(const Model&) = delete;
@@ -31,11 +36,18 @@ namespace Zenith {
 
 		private:
 			void createVertexBuffers(const std::vector<Vertex>& vertices);
+			void createIndexBuffer(const std::vector<uint32_t>& indices);
 
 			Device& device;
+
 			VkBuffer vertexBuffer;
 			VkDeviceMemory vertexBufferMemory;
 			uint32_t vertexCount;
+
+			bool hasIndexBuffer = false;
+			VkBuffer indexBuffer;
+			VkDeviceMemory indexBufferMemory;
+			uint32_t indexCount;
 		};
 	}
 }
